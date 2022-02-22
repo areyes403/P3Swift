17b0cd5b5d14719cc0e3cb7656410ac099dc328b
areyes403 <abrahamreyes321@hotmail.com>
2022-02-22 06:38:35 -0800

    Initial Commit

---
+++ Practica3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica3
+//
+//  Created by Abraham Sahid Reyes Rodriguez on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
